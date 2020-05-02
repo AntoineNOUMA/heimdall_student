563aea542ddaa061d3d72ac6a24c63c9da0e3837
antoinenouma <antoinenouma@gmail.com>
2020-05-02 15:15:21 +0200

    maj

--- js/student_homepage.js
+++ js/student_homepage.js
@@ -49,4 +49,41 @@ $(document).ready(function(){
         $(this).parents("tr").remove();
 		$(".add-new").removeAttr("disabled");
     });
+    
+});
+
+//Check all checkbox
+$("#checkAll").click(function () {
+    $(".custom-control-input").prop('checked', $(this).prop('checked'));
+});
+
+
+function bs_input_file() {
+    $(".input-file").before(
+        function() {
+            if ( ! $(this).prev().hasClass('input-ghost') ) {
+                var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+                element.attr("name",$(this).attr("name"));
+                element.change(function(){
+                    element.next(element).find('input').val((element.val()).split('\\').pop());
+                });
+                $(this).find("button.btn-choose").click(function(){
+                    element.click();
+                });
+                $(this).find("button.btn-reset").click(function(){
+                    element.val(null);
+                    $(this).parents(".input-file").find('input').val('');
+                });
+                $(this).find('input').css("cursor","pointer");
+                $(this).find('input').mousedown(function() {
+                    $(this).parents('.input-file').prev().click();
+                    return false;
+                });
+                return element;
+            }
+        }
+    );
+}
+$(function() {
+    bs_input_file();
 });
